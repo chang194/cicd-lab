3f2f232d319cbd2515dc1c17513c72069db8604d
chang194 <134861313+chang194@users.noreply.github.com>
2026-05-16 17:16:54 +0000

    feat: fix version info (type correct)

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export function buildApp(options: FastifyServerOptions = {}) {
   app.get('/', async () => {
     return {
       message: 'CI/CD Lab Fastify app is running',
-      version: getVersionInfo(process.env.APP_VERSION || 'dev')  // string 傳給 number 參數 → 型別錯誤
+      version: getVersionInfo(process.env.APP_VERSION || 'dev') // string 傳給 number 參數 → 型別錯誤
     };
   });
 
